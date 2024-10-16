ea5c5d01d4d476b3f2ed68e337789f87d130fbf8
PratyutCS <pratyut987@gmail.com>
2024-10-17 00:19:37 +0530

    styled and added conditional rendering

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,6 +4,12 @@ use forensic_rs::prelude::*;
 use frnsc_hive::reader::HiveRegistryReader;
 use frnsc_hive::reader::open_hive_with_logs;
 
+static mut SYSTEM : bool = false;
+static mut SOFTWARE : bool = false;
+static mut SECURITY : bool = false;
+static mut SAM : bool = false;
+// static mut fileslol;
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
@@ -11,17 +17,27 @@ fn greet(name: &str) -> String {
 
 pub fn from_fs_custom(mut reader: HiveRegistryReader, mut fs: Box<dyn VirtualFileSystem>) -> ForensicResult<Box<dyn RegistryReader>> {
     let config_folder = std::path::Path::new(".\\");
+    unsafe{
+        SYSTEM = false;
+        SOFTWARE = false;
+        SECURITY = false;
+        SAM = false;
+    }
     if let Some(hive) = open_hive_with_logs(&mut fs, config_folder, "SYSTEM") {
         reader.set_system(hive);
+        unsafe {SYSTEM = true};
     }
     if let Some(hive) = open_hive_with_logs(&mut fs, config_folder, "SOFTWARE") {
         reader.set_software(hive);
+        unsafe {SOFTWARE = true};
     }
     if let Some(hive) = open_hive_with_logs(&mut fs, config_folder, "SECURITY") {
         reader.set_security(hive);
+        unsafe {SECURITY = true};
     }
     if let Some(hive) = open_hive_with_logs(&mut fs, config_folder, "SAM") {
         reader.set_sam(hive);
+        unsafe {SAM = true};
     }
     match reader.load_user_hives(&mut fs) {
         Ok(_) => {}
@@ -37,41 +53,87 @@ pub fn from_fs_custom(mut reader: HiveRegistryReader, mut fs: Box<dyn VirtualFil
 }
 
 #[tauri::command]
+fn fileexists(filepath: &str) -> String{
+    let fs = Box::new(forensic_rs::core::fs::ChRootFileSystem::new(filepath, Box::new(forensic_rs::core::fs::StdVirtualFS::new())));
+    let hreader = HiveRegistryReader::new();
+    from_fs_custom(hreader, fs).unwrap();
+    unsafe{
+        format!("{:?}:{:?}:{:?}:{:?}",SYSTEM, SOFTWARE, SECURITY, SAM)
+    }
+}
+
+#[tauri::command]
 fn hivereader(filepath: &str) -> String {
     let fs = Box::new(forensic_rs::core::fs::ChRootFileSystem::new(filepath, Box::new(forensic_rs::core::fs::StdVirtualFS::new())));
     let hreader = HiveRegistryReader::new();
     let reader = from_fs_custom(hreader, fs).unwrap();
     let res = reader.open_key(HKLM, r"SAM\Domains\Account\Users\Names");
+    let mut s = String::new();
     
     if res.is_err() {
         println!("Hive file not found! {:?}", res.err());
         return "Error Occured!".to_string();
     }
-
-    let user_names_key = reader.open_key(HKLM, r"SAM\Domains\Account\Users\Names").expect("Should list all user names");
-    let users = reader.enumerate_keys(user_names_key).expect("Should enumerate users");
-
-    let network_cards = reader.open_key(HKLM, r"SOFTWARE\Microsoft\Windows NT\CurrentVersion\NetworkCards").unwrap();
-    let network_cards_values = reader.enumerate_keys(network_cards).unwrap();
-
-    let run_apps = reader.open_key(HKLM, r"SOFTWARE\RegisteredApplications").unwrap();
-    let run_apps_values = reader.enumerate_values(run_apps).unwrap();
-
-    println!("\n---/ Registered Applications /----");
-    for value in run_apps_values.iter() {
-        let val = reader.read_value(run_apps, &value).unwrap();
-        println!("{:?} : {:?}", value, val);
+    let user_names_key;
+    let users;
+    if unsafe{SAM} {
+        user_names_key = reader.open_key(HKLM, r"SAM\Domains\Account\Users\Names").expect("Should list all user names");
+        users = reader.enumerate_keys(user_names_key).expect("Should enumerate users");
+        println!("Users: {:?}", users);
+        s.push_str(&users.join(", "));
+    }
+    s.push_str(":");
+    if unsafe{SOFTWARE} {
+        let network_cards = reader.open_key(HKLM, r"SOFTWARE\Microsoft\Windows NT\CurrentVersion\NetworkCards").unwrap();
+        let network_cards_values = reader.enumerate_keys(network_cards).unwrap();
+        println!("Network Cards: {:?}", network_cards_values);
+        s.push_str(&network_cards_values.join(", "));
+    
+        let run_apps = reader.open_key(HKLM, r"SOFTWARE\RegisteredApplications").unwrap();
+        let run_apps_values = reader.enumerate_values(run_apps).unwrap();
+    
+        println!("\n---/ Registered Applications /----");
+        for value in run_apps_values.iter() {
+            let val = reader.read_value(run_apps, &value).unwrap();
+            println!("{:?} : {:?}", value, val);
+        }
+        s.push_str(&run_apps_values.join(", "));
     }
 
-    println!("Network Cards: {:?}", network_cards_values);
-    println!("Users: {:?}", users);
+    // println!("Network Cards: {:?}", network_cards_values);
+    // println!("Users: {:?}", users);
     // assert_eq!("Administrador", users[0]);
     // assert_eq!("DefaultAccount", users[1]);
     // assert_eq!("Invitado", users[2]);
     // assert_eq!("maria.feliz.secret", users[3]);
     // assert_eq!("pepe.contento.secret", users[4]);
     // assert_eq!("SuperSecretAdmin", users[5]);
-    format!("Users: {:?} \n Network Cards: {:?} Run apps: {:?}", users, network_cards_values, run_apps_values)
+    // format!("Users: {:?} \n Network Cards: {:?} Run apps: {:?}", users, network_cards_values, run_apps_values)
+    // println!("{}", s);
+    format!("{}", s)
+}
+
+#[tauri::command]
+fn hive_sam_reader(filepath: &str) -> String {
+    let fs = Box::new(forensic_rs::core::fs::ChRootFileSystem::new(filepath, Box::new(forensic_rs::core::fs::StdVirtualFS::new())));
+    let hreader = HiveRegistryReader::new();
+    let reader = from_fs_custom(hreader, fs).unwrap();
+    let res = reader.open_key(HKLM, r"SAM\Domains\Account\Users\Names");
+    let mut s = String::new();
+    
+    if res.is_err() {
+        println!("Hive file not found! {:?}", res.err());
+        return "Error Occured!".to_string();
+    }
+    let user_names_key;
+    let users;
+    if unsafe{SAM} {
+        user_names_key = reader.open_key(HKLM, r"SAM\Domains\Account\Users\Names").expect("Should list all user names");
+        users = reader.enumerate_keys(user_names_key).expect("Should enumerate users");
+        println!("Users: {:?}", users);
+        s.push_str(&users.join(", "));
+    }
+    format!("{}", s)
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -80,7 +142,7 @@ pub fn run() {
         .plugin(tauri_plugin_log::Builder::new().build())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet, hivereader])
+        .invoke_handler(tauri::generate_handler![greet, hivereader, fileexists, hive_sam_reader])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/pages/secondPage.jsx
+++ src/pages/secondPage.jsx
@@ -1,19 +1,36 @@
 import { useLocation, useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { invoke } from '@tauri-apps/api/core';
+import "../css/sp.css";
 
 function NewPage() {
   const location = useLocation();
   const navigate = useNavigate();
   const [filePath, setFilePath] = useState(location.state?.filePath);
-  const [greetingMessage, setGreetingMessage] = useState('');
+  const [Message, setMessage] = useState('');
+  const [System, setSystem] = useState(false);
+  const [Software, setSoftware] = useState(false);
+  const [Security, setSecurity] = useState(false);
+  const [Sam, setSam] = useState(false);
 
   useEffect(() => {
     if (filePath) {
-      invoke('hivereader', { filepath: filePath })
+      invoke('fileexists', { filepath: filePath })
         .then((message) => {
           console.log(message);
-          setGreetingMessage(message);
+          const split = message.split(":");
+          if (split[0] != "false") {
+            setSystem(true);
+          }
+          if (split[1] != "false") {
+            setSoftware(true);
+          }
+          if (split[2] != "false") {
+            setSecurity(true);
+          }
+          if (split[3] != "false") {
+            setSam(true);
+          }
         })
         .catch((error) => console.error('Error invoking greet:', error));
     }
@@ -22,19 +39,37 @@ function NewPage() {
   const handleBackClick = () => {
     navigate(-1);
   };
+  const samReader = () =>{
+    invoke('hive_sam_reader', { filepath: filePath })
+    .then((message) => {
+      console.log(message);
+      setMessage(message);
+    })
+    .catch((error) => console.error('Error invoking greet:', error));
+  };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-3xl font-bold">File Selected</h1>
-      {filePath ? (
-        <p className="mt-4">You selected: {filePath}</p>
-      ) : (
-        <p>No file selected.</p>
-      )}
-      {greetingMessage && <p className="mt-4">{greetingMessage}</p>}
-      
-      <h1 className="text-3xl font-bold">fking works!!!!..</h1>
-      <button onClick={handleBackClick} className="btn btn-primary">Go Back</button>
+    <div className='ini'>
+      <div className='details'>
+        <button onClick={handleBackClick} className="back">Go Back</button>
+        <div className='foldername'>
+          {filePath ? (
+            <p className="mt-4">Folder selected: <span className='path'>{filePath}</span></p>
+          ) : (
+            <p>No file selected.</p>
+          )}</div>
+      </div>
+      <div className="body">
+        <div className='div1'>
+          {System && <button className='button'>SYSTEM</button>}
+          {Software && <button className='button'>SOFTWARE</button>}
+          {Security && <button className='button'>SECURITY</button>}
+          {Sam && <button onClick={samReader} className='button' >SAM</button>}
+        </div>
+        <div className='div2'>
+          {Message && <p className="mt-4">{Message}</p>}
+        </div>
+      </div>
     </div>
   );
 }
