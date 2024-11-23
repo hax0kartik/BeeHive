55cc7b9fe8f4e4b24a2a360eca0d45fad0f5d316
hax0kartik <agarwala.kartik@gmail.com>
2024-11-23 17:13:52 +0530

    Implement most of diff view

--- src/pages/diff.jsx
+++ src/pages/diff.jsx
@@ -14,100 +14,119 @@ function Diff() {
   const navigate = useNavigate();
 
   const location = useLocation();
-  const [filePath1, setFilePath1] = useState("/home/kartika/Desktop/projects/Beehive/tests/prat_hives/prat/security");
-  const [filePath2, setFilePath2] = useState("/home/kartika/Desktop/projects/Beehive/tests/raghav_hives/raghav/Security");
+  const [filePath1, setFilePath1] = useState(location.state?.filePath1);
+  const [filePath2, setFilePath2] = useState(location.state?.filePath2);
   const [tree, setTree] = useState();
-  const [rowData, setRowData] = useState([
-    { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+  const [rowData, setRowData] = useState([]);
+  // Column Definitions: Defines the columns to be displayed.
+  const [colDefs, setColDefs] = useState([
+    { field: "Key" },
+    { field: "Change Type" },
+    { field: "Value Name" },
+    { field: "Old Value" },
+    { field: "New Value" }
   ]);
 
   const build_diff_nodes = async () => {
     let res = await invoke('t_hive_differ', { filepath1: filePath1, filepath2: filePath2 });
     warn("Back in build_diff_nodes");
-    warn("Length:" + res["nodes"].length);
 
-    const diff = createPatch(res["nodes"][1], res["nodes"][0]);
+    const diff = createPatch(res["nodes"][0], res["nodes"][1]);
 
-    let initial_tree = res["nodes"][0];
-    warn(JSON.stringify(diff[0], null, 2));
-    warn(JSON.stringify(diff[1], null, 2));
-    warn(JSON.stringify(diff, null, 2));
+    let newRowData = [];
+
+    for (let i = 0; i < diff.length; i++) {
+      if (diff[i]["op"] === "remove") {
+        let key = diff[i]["path"].replaceAll("/children", "");
+        let data = {
+          "Key" : key,
+          "Change Type" : "Removed",
+          "Value Name": "",
+          "Old Value": "",
+          "New Value" : ""  
+        }
+
+        newRowData.push(data);
+      } else if (diff[i]["op"] === "add") {
+        let key = diff[i]["path"].replaceAll("/children", "");
+        let data = {
+          "Key" : key,
+          "Change Type" : "New Key",
+          "Value Name": "",
+          "Old Value": "",
+          "New Value" : ""  
+        }
+        newRowData.push(data);
+
+        const recursiveAdd = (children) => {
+          Object.keys(children).map((key) => {
+            newRowData.push({
+              "Key" : children[key]["path"].replaceAll("//ROOT", ""),
+              "Change Type" : "New Key",
+              "Value Name": "",
+              "Old Value": "",
+              "New Value" : "",
+            });
+
+          Object.keys(children[key]["values"]).map((keyvalue) => {
+            newRowData.push({
+              "Key" : children[key]["path"].replaceAll("//ROOT", ""),
+              "Change Type" : "New Value",
+              "Value Name": keyvalue,
+              "Old Value": "",
+              "New Value" : children[key]["values"][keyvalue]
+            });
+          });
+
+             if (children[key]["has_children"]) {
+               recursiveAdd(children[key]["children"]);
+             }
+          })
+        }
+
+        recursiveAdd(diff[i]["value"]["children"]);
+
+      } else {
+        warn("Not handled : " + diff[i]["op"]);
+      }
+    }
+
+    setRowData(_ => ([...newRowData]));
+
+    //warn(JSON.stringify(diff, null, 2));
   }
 
   const dummy = async (e, f) => {
 
   }
 
-  useEffect(() => {
+  useEffect( async () => {
     build_diff_nodes();
   }, []);
 
   const content = () => {
-    
-    // Column Definitions: Defines the columns to be displayed.
-    const [colDefs, setColDefs] = useState([
-      { field: "Key" },
-      { field: "Change Type" },
-      { field: "Value Name" },
-      { field: "Old Value" },
-      { field: "New Value" }
-    ]);
-     
-    // return (
-    //   <div className="flex h-full items-center text-center justify-center gap-3">
-    //     <h1 className="text-2xl">Generating diff...</h1>
-    //     <span className="loading loading-spinner loading-lg"></span>
-    //   </div>
-    // )
+    warn("rowData length: " + rowData.length);
+    if (rowData.length === 0) {
+        return (
+          <div className="flex h-full items-center text-center justify-center gap-3">
+            <h1 className="text-2xl">Generating diff...</h1>
+            <span className="loading loading-spinner loading-lg"></span>
+          </div>
+        )
+    } else {
 
-    return (
-      <div
-        className="ag-theme-quartz-auto-dark" // applying the Data Grid theme
-        style={{ height: "inherit", width: "inherit" }} // the Data Grid will fill the size of the parent container
-      >
-        <AgGridReact
-          rowData={rowData}
-          columnDefs={colDefs}
-        />
-      </div>
-    )
-
-    return (
-      <div className="flex h-full">
-        <div className='flex align-items flex-col pl-6 pt-6 gap-4 w-2/5'>
-          <ul className="menu menu-xs bg-base-200 rounded-lg w-full overflow-x-auto overflow-y-auto h-5/6">
-            {
-              Object.entries(tree).map(entry => {
-                return addMenuEntries(entry[0], entry[1]);
-              })
-            }
-          </ul>
+      return (
+        <div
+          className="ag-theme-quartz-auto-dark" // applying the Data Grid theme
+          style={{ height: "inherit", width: "inherit" }} // the Data Grid will fill the size of the parent container
+        >
+          <AgGridReact
+            rowData={rowData}
+            columnDefs={colDefs}
+          />
         </div>
-        <div className="divider divider-horizontal"></div>
-        <div className='w-full'>
-          <table className='table table-fixed'>
-            <thead>
-              <tr className=''>
-                <th className='text-base'> Key </th>
-                <th className='text-base'> Value </th>
-              </tr>
-            </thead>
-            <tbody>
-              {
-                Object.entries(Table).map(entry => {
-                  return (
-                    <tr>
-                      <th> {entry[0]} </th>
-                      <th> {entry[1]} </th>
-                    </tr>
-                  )
-                })
-              }
-            </tbody>
-          </table>
-        </div>
-      </div>
-    )
+      )
+    }
   }
 
   return (
